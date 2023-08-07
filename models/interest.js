d466736786fad67e60f8fdd45c64ee0d024aa52b
Vishal Kondle <vishal.kondle@gmail.com>
2023-08-07 21:44:49 +0530

    interest model and api

---
+++ models/interest.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+var Schema = mongoose.Schema;
+
+var interestSchema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    from_days: {
+      type: Number,
+      required: true,
+    },
+    to_days: {
+      type: Number,
+      required: true,
+    },
+    interest: {
+      type: Number,
+      required: true,
+    },
+    comments: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+mongoose.models = {};
+
+global.Interest = global.Interest || mongoose.model("Interest", interestSchema);
+
+export default global.Interest;
---
+++ pages/api/interest.js
@@ -0,0 +1,103 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+import connectDB from "../../middleware/mongodb";
+import Interest from "../../models/interest";
+
+const handler = async (req, res) => {
+  // const session = await getServerSession(req, res, authOptions);
+  // if (!session?.user?.isAdmin) {
+  //   return res
+  //     .status(401)
+  //     .json({ error: true, message: "You are unauthorized!" });
+  // }
+  if (req.method === "POST") {
+    try {
+      let interests = await Interest.find().sort({ _id: -1 }).limit(1);
+      let interest = await Interest({
+        ...req.body,
+        id: (interests[0]?.id || 0) + 1,
+      });
+      await interest.save();
+      interests = await Interest.find();
+      return res.status(200).json({
+        error: false,
+        ok: true,
+        data: interests,
+        message: "Interest created successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        ok: false,
+        data: error.message,
+        message: "Error while creating interest",
+      });
+    }
+  }
+  if (req.method === "GET") {
+    try {
+      let interest;
+      if (req.query?.days) {
+        interest = await Interest.find({
+          from_days: { $lte: req.query.days },
+          to_days: { $gte: req.query.days },
+        });
+      } else {
+        interest = await Interest.find();
+      }
+      return res.status(200).json({
+        error: false,
+        ok: true,
+        data: interest,
+        message: "Interest fetched successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        ok: false,
+        data: error.message,
+        message: "Error while fetching interest",
+      });
+    }
+  }
+  if (req.method === "PUT") {
+    try {
+      await Interest.findOneAndUpdate({ id: req.query.id }, { ...req.body });
+      var interests = await Interest.find();
+      return res.status(200).json({
+        error: false,
+        ok: true,
+        data: interests,
+        message: "Interest updated successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        ok: false,
+        data: error.message,
+        message: "Error while updating loans",
+      });
+    }
+  }
+  if (req.method === "DELETE") {
+    try {
+      await Interest.findOneAndDelete({ id: req.query.id });
+      var interests = await Interest.find();
+      return res.status(200).json({
+        error: false,
+        ok: true,
+        data: interests,
+        message: "Interest deleted successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        ok: false,
+        data: error.message,
+        message: "Error while deleting interest",
+      });
+    }
+  }
+};
+
+export default connectDB(handler);
